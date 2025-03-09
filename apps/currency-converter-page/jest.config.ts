e1622dc33f62cea5b45ca317ec62000902f7ce2b
thurakkal92 <nthurakkal92@gmail.com>
2025-03-09 12:37:22 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/HW7wuhLPAC

---
+++ apps/currency-converter-page/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'currency-converter-page',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/currency-converter-page',
+};
